6cc8c747224d875065110b566682d6b0a869737b
ognjenr02 <123863488+ognjenr02@users.noreply.github.com>
2023-11-09 18:13:47 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,83 @@
+import { StyleSheet, View, Button, FlatList } from 'react-native';
+import React, { FC, useState } from 'react';
+import TaskItem from './components/TaskItem';
+import TaskInput from './components/TaskInput';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from './constants';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+interface Task {
+  text: string;
+  id: string;
 }
 
+const App: FC = () => {
+  const [currentTasks, setTasks] = useState<Task[]>([]);
+  const [modalIsVisible, setModalIsVisible] = useState<boolean>(false);
+
+  function startAddTaskHandler() {
+    setModalIsVisible(true);
+  }
+
+  function endAddTaskHandler() {
+    setModalIsVisible(false);
+  }
+
+  function addTaskHandler(enteredText: string) {
+    setTasks((currentTasks) => [
+      ...currentTasks,
+      { text: enteredText, id: Math.random().toString() },
+    ]);
+    endAddTaskHandler();
+  }
+
+  function deleteTaskHandler(id: string) {
+    setTasks((currentTasks) => {
+      return currentTasks.filter((item) => item.id !== id);
+    });
+  }
+
+  return (
+    <>
+      <StatusBar style="light" />
+      <View style={styles.appContainer}>
+        <Button title="Add New Task" onPress={startAddTaskHandler} />
+        <TaskInput
+          onAddTask={addTaskHandler}
+          visible={modalIsVisible}
+          onCancel={endAddTaskHandler}
+        />
+        <View style={styles.taskContainer}>
+          <FlatList
+            data={currentTasks}
+            keyExtractor={(item, index) => {
+              return item.id;
+            }}
+            renderItem={(itemData) => {
+              return (
+                <TaskItem
+                  text={itemData.item.text}
+                  id={itemData.item.id}
+                  onDeleteItem={deleteTaskHandler}
+                />
+              );
+            }}
+          />
+        </View>
+      </View>
+    </>
+  );
+};
+
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    backgroundColor: Colors.primary_background,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  taskContainer: {
+    flex: 4,
   },
 });
+
+export default App;
